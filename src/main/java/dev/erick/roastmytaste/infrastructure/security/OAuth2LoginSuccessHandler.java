630f86bc1c06443647b79ead72405f2741ead754
Erick Murakami <erickmurakami.ehm@gmail.com>
2026-05-26 16:02:52 -0300

    fix(security): removed cookie behaviour

--- src/main/java/dev/erick/roastmytaste/infrastructure/security/OAuth2LoginSuccessHandler.java
+++ src/main/java/dev/erick/roastmytaste/infrastructure/security/OAuth2LoginSuccessHandler.java
@@ -1,6 +1,5 @@
 package dev.erick.roastmytaste.infrastructure.security;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,7 +8,7 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.web.util.UriComponentsBuilder; // pode remover se não usar mais
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 
@@ -36,18 +35,18 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
                 oauthToken.getName()
         );
 
+        if (client == null || client.getAccessToken() == null) {
+            response.sendRedirect(frontendUrl + "?error=token");
+            return;
+        }
+
         String spotifyAccessToken = client.getAccessToken().getTokenValue();
 
-        Cookie cookie = new Cookie("spotify_token", spotifyAccessToken);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(3600);
-        cookie.setAttribute("SameSite", "None");
-        cookie.setDomain("api-roast.erickkami.dev");
+        String targetUrl = UriComponentsBuilder.fromUriString(frontendUrl)
+                .queryParam("token", spotifyAccessToken)
+                .build()
+                .toUriString();
 
-        response.addCookie(cookie);
-
-        response.sendRedirect(frontendUrl);
+        response.sendRedirect(targetUrl);
     }
 }
